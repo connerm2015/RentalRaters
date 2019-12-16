c97942d5c658ac0bf48f4b0dbd3962c8a3ae8c15
connerm2015 <58260938+connerm2015@users.noreply.github.com>
2019-12-15 19:39:48 -0500

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.add('landlordprofile.html');
+    })
+  );
+});
+ 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
